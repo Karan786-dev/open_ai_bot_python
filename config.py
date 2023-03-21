700fbd20e410eef3a2869159633ec0a3b6325c7c
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2023-03-21 14:30:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-bot_token='6275129590:AAHR_yAMcjd6pFyM9nw18Y5RJAh2Y8FldPA'
-openai_key='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'
-webhook_url='https://0e6b-106-206-230-82.in.ngrok.io'
-telegram_bot_api = 'https://api.telegram.org/bot'
+bot_token='Bot Token'
+openai_key='Your open ai key'
+webhook_url='Webhook Url'
+telegram_bot_api = 'https://api.telegram.org/bot' 
